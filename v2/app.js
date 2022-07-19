05ce429ad68adee5bdcb40d0f8122fe0bd4aa4ca
Schirlei Alves <89231147+Schirlei@users.noreply.github.com>
2022-07-19 20:01:57 -0300

    segunda versão

---
+++ v2/app.js
@@ -0,0 +1,28 @@
+let swiperText = new Swiper(".text", {
+  grabCursor: true,
+  slidesPerView: 1.15,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let swiperImages = new Swiper(".images", {
+  effect: "fade",
+});
+
+function sincronizarImagem() {
+  let index = swiperText.activeIndex;
+  swiperImages.slideTo(index);
+}
+
+function sincronizarTexto() {
+  let index = swiperImages.activeIndex;
+  swiperText.slideTo(index);
+}
+
+swiperText.on("slideChange", sincronizarImagem);
+swiperImages.on("slideChange", sincronizarTexto);
